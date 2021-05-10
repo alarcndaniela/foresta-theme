f504bb5eacf721b4054a6b6af1a542e679c1b545
Daniela Alarcon <daniela.alarcon@ucr.ac.cr>
2021-05-09 21:25:49 -0600

    cambio de prueba

--- header-image.php
+++ header-image.php
@@ -55,7 +55,7 @@
 
             </div>
             <div class="row">
-                    <h1><span class="around-title">Crafts form our hands</span><br><span class="around-title">to yours</span></h1>
+                    <h1><span class="around-title">Crafts form our hands</span><br><span class="around-title">to your hands</span></h1>
             </div>
 
         </header>
