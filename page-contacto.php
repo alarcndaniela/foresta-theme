61176fc9fe918bcd9133194b5efcfcc1e59e5a8a
Daniela Alarcon <daniela.alarcon@ucr.ac.cr>
2021-06-04 19:57:31 -0600

    custom_fields_contact

--- page-contacto.php
+++ page-contacto.php
@@ -1,12 +1,12 @@
 <?php get_header() ?>
    
    <div class="row contact justify-content-start"
-   style="background:url(<?php echo get_template_directory_uri()."/img/contact-header.png" ?>);
+   style="background:url(<?php the_field('imagen_portada');?>);
    height: 60vh; 
    background-size:cover; 
    background-position:right" >
       <div class="row justify-content-center align-content-center align-self-center">
-         <h1><span class="around-title">¡Empecemos una conversación!</span></h1>
+         <h1><span class="around-title"><?php the_field('mensaje_portada');?></span></h1>
       </div>    
    </div>
 
@@ -14,35 +14,35 @@
       <div class="row contact-info justify-content-center" data-aos="fade-right" data-aos-duration="2000">
          <div class="col-12 p-5 contact-info-1">
             <div class="info p-5">
-            <h1 class="contact-title"><span>Para nosotros será un placer ayudarte, cuéntanos como podemos hacerlo</span></h1>
+            <h1 class="contact-title"><span><?php the_field('texto_bienvenida');?></span></h1>
             <br>
-            <div class="col my-4">    
-               <img class="m-1" id="phone" src="<?php echo get_template_directory_uri()."/img/phone.svg" ?> " width="20px";>
-               <p class="contact-text"><span>2660-3678 / 8735-9023</span></p>
-            </div>
-  
-            <div class="col my-4">
-               <img class="m-1" id="location" src="<?php echo get_template_directory_uri()."/img/location.svg" ?> " width="20px";>
-               <p class="contact-text"><span>100 mts este de la plaza de deportes de Monteverde, Puntarenas.</span></p>
-            </div>
-      
-            <div class="col my-4">
-               <img class="m-1" id="clock" src="<?php echo get_template_directory_uri()."/img/clock.svg" ?> " width="20px";>
-               <p class="contact-text"><span>Lunes - Viernes<br>10:00 a.m. - 5:00 p.m.</span></p>
-            </div>
-            
-            
-            <div class="col my-4">
-               <p class="contact-text"><span>Síguenos</span></p>
-               <img class="m-1" id="facebook2" src="<?php echo get_template_directory_uri()."/img/facebook.svg" ?> " width="20px";>
-               <img class="m-1" id="instagram2" src="<?php echo get_template_directory_uri()."/img/instagram.svg" ?> " width="20px";>
-            </div>
+               <div class="col my-4">    
+                  <img class="m-1" id="phone" src="<?php echo get_template_directory_uri()."/img/phone.svg" ?> " width="20px";>
+                  <p class="contact-text"><span><?php the_field('numero_telefonico');?></span></p>
+               </div>
+   
+               <div class="col my-4">
+                  <img class="m-1" id="location" src="<?php echo get_template_directory_uri()."/img/location.svg" ?> " width="20px";>
+                  <p class="contact-text"><span><?php the_field('ubicacion');?></span></p>
+               </div>
+         
+               <div class="col my-4">
+                  <img class="m-1" id="clock" src="<?php echo get_template_directory_uri()."/img/clock.svg" ?> " width="20px";>
+                  <p class="contact-text"><span><?php the_field('horario');?></span></p>
+               </div>
+               
+               
+               <div class="col my-4">
+                  <p class="contact-text"><span>Síguenos</span></p>
+                  <img class="m-1" id="facebook2" src="<?php the_field('link_facebook');?> " width="20px";>
+                  <img class="m-1" id="instagram2" src="<?php the_field('link_instagram');?> " width="20px";>
+               </div>
             </div>
             
          </div>
 
          <div class="col-12 contact-info-2 justify-content-center align-self-center">
-            <iframe id="waze" src="https://embed.waze.com/iframe?zoom=14&lat=10.313399&lon=-84.816460&ct=livemap&pin=1" allowfullscreen></iframe>
+            <iframe id="waze" src="<?php the_field('link_waze');?> " allowfullscreen></iframe>
          </div>
          
       </div>
