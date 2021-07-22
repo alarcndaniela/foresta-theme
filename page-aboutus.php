ffa5d3fc866ef838b0a2a1bbb848b0edbf7fd343
Daniela Alarcon <daniela.alarcon@ucr.ac.cr>
2021-07-21 22:12:48 -0600

    content3

--- page-aboutus.php
+++ page-aboutus.php
@@ -70,8 +70,8 @@
       </div>
    </section>
    <section>
+   <h2 class="founders-title">The hands behind everything</h2>
    <div class="row founders justify-content-center p-5">
-      <h2 class="founders-title">The hands behind everything</h2>
       <div class="col-6 px-5 text-center">
          <img src="/img/china.jpg" class="rounded-circle img-fluid w-50" alt="...">
          <h3 class="pt-2"><?php the_field('nombre_1');?></h3>
